907edd60d80416fcdf583e435f0101a6279b6655
Jonas Ehrlich <jonas.ehrlich@gmail.com>
2024-10-07 19:49:08 +0200

    wip: Add stage dependency checks

--- installer/stages.py
+++ installer/stages.py
@@ -21,7 +21,10 @@ class ActionsContainer(Protocol):
     def add_argument(*args: Any, **kwargs: Any) -> Any: ...
 
 
-StageFunc = collections.abc.Callable[[Config], Any]
+class StageFunc(Protocol):
+    __name__: str
+
+    def __call__(self, cfg: Config) -> Any: ...
 
 
 class _StageRegistry:
@@ -36,6 +39,15 @@ class _StageRegistry:
 
     def __iter__(self) -> collections.abc.Iterator[Stage]:
         return iter(self._stages.values())
+
+    def resolve_dependencies(self) -> None:
+        """Resolve dependencies in all stages"""
+        for _stage in self._stages.values():
+            _stage.resolve_dependencies(self._stages)
+
+    @classmethod
+    def clear(cls) -> None:
+        cls._stages.clear()
 
 
 STAGE_REGISTRY = _StageRegistry()
@@ -58,8 +70,8 @@ class Stage:
         interactive_confirm: bool,
         predicate: collections.abc.Callable[[], bool] | None,
         abort_on_error: bool,
-        details: str | None = None,
-        platforms: collections.abc.Sequence[Platform] | None = None,
+        details: str | None,
+        platforms: collections.abc.Sequence[Platform] | None,
         dependencies: list[str | Stage] | None = None,
     ) -> None:
         self._name = name
@@ -67,7 +79,9 @@ class Stage:
         self._func = func
         self._details = details
         self._platforms = platforms or []
-        self._dependencies = dependencies or []
+
+        self._raw_dependencies = dependencies or []
+        self._resolved_dependencies: list[Stage] = []
 
         self._interactive_confirm = interactive_confirm
         self._predicate = predicate
@@ -98,6 +112,36 @@ class Stage:
     def is_valid_for_current_platform(self) -> bool:
         """Return whether this stage is supported on the current platform"""
         return not self._platforms or platform.system() in self._platforms
+
+    def func_name(self) -> str:
+        return self._func.__name__
+
+    def resolve_dependencies(self, stage_map: dict[str, Stage]) -> None:
+        """Resolve the string dependencies to
+
+        :param stage_map: Map of flag names to stage objects
+        """
+        func_name_map = {stage.func_name: stage for stage in stage_map.values()}
+
+        for dep in enumerate(self._raw_dependencies):
+            if isinstance(dep, str):
+                # The stage was only provided as a string, replace all dependency names with their real stages
+
+                stage = stage_map.get(dep)
+                if not stage:
+                    stage = func_name_map.get(dep)
+
+                if stage:
+                    self._resolved_dependencies.append(stage_map[dep])
+
+                self._logger.warning(
+                    "Dependency '%s' defined on '%s' is not a valid stage, ignoring", dep, self.flag_name
+                )
+                continue
+            elif isinstance(dep, Stage):
+                self._resolved_dependencies.append(dep)
+            else:
+                self._logger.warning("Invalid type %s for %r", type(dep), dep)
 
     def __call__(self, cfg: Config) -> None:
         flag_name = self.flag_name
@@ -140,13 +184,15 @@ class Stage:
             _logger.info(f"{self._name} - done")
 
 
-def stage(
+def stage(  # noqa: PLR0913
     name: str,
+    help: str | None = None,
     interactive_confirm: bool = False,
     predicate: collections.abc.Callable[[], bool] | None = None,
     abort_on_error: bool = False,
     details: str | None = None,
     platforms: collections.abc.Sequence[Platform] | None = None,
+    dependencies: list[str | Stage] | None = None,
 ) -> collections.abc.Callable[..., Stage]:
     """Make a function to wrap a function as a stage of the installation
 
@@ -168,6 +214,7 @@ def stage(
             abort_on_error=abort_on_error,
             details=details,
             platforms=platforms,
+            dependencies=dependencies,
         )
         _StageRegistry.register(_stage)
         return _stage
